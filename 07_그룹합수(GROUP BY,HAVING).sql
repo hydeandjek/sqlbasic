eb47ea8cb2e94973c110cc69d519cd9161f6ad43
hydeandjek <hydeandjek@naver.com>
2023-09-25 17:31:03 +0900

    9/25 수업

--- 07_그룹합수(GROUP BY,HAVING).sql
+++ 07_그룹합수(GROUP BY,HAVING).sql
@@ -91,7 +91,34 @@ FROM employees
 GROUP BY TO_CHAR(hire_date,'YYYY')
 ORDER BY 입사년도 ASC;
 
+/*
+문제 3.
+급여가 5000 이상인 사원들의 부서별 평균 급여를 출력하세요. 
+단 부서 평균 급여가 7000이상인 부서만 출력하세요.
+*/
+SELECT
+    department_id,
+    AVG(salary) AS 평균급여
+FROM employees
+WHERE salary >=5000
+HAVING AVG(salary)>=7000
+GROUP BY department_id;
 
+/*
+문제 4.
+사원 테이블에서 commission_pct(커미션) 컬럼이 null이 아닌 사람들의
+department_id(부서별) salary(월급)의 평균, 합계, count를 구합니다.
+조건 1) 월급의 평균은 커미션을 적용시킨 월급입니다.
+조건 2) 평균은 소수 2째 자리에서 절삭 하세요.
+*/
+SELECT
+    department_id,
+    TRUNC(AVG(salary*(commission_pct+1)),2)AS 월급평균,
+    SUM(salary*(commission_pct+1)) AS 월급합계,
+    count(*)
+FROM employees
+WHERE commission_pct IS NOT NULL
+GROUP BY department_id;
 
 
 
